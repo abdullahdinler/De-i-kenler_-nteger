c95cb376e2793cbbfc0575ceb2c76bfbe881b5cc
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2022-03-18 16:20:38 +0300

    Add project files.

---
+++ Değişkenler_İnteger/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Değişkenler_İnteger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kisa_kenar, uzun_kenar, alan, cevre;
+            kisa_kenar = Convert.ToInt16(textBox1.Text);
+            uzun_kenar = Convert.ToInt16(textBox2.Text);
+            alan = kisa_kenar * uzun_kenar;
+            cevre = 2 * (kisa_kenar + uzun_kenar);
+            listBox1.Items.Add("Alan:  " + alan + "  Cevre: " + cevre);
+        }
+    }
+}
